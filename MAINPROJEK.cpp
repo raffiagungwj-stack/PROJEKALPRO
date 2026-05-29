cd0c89ce227af59fd3e225eaa509b7912127c766
Bahlil_Kader <mzenitho.mp@gmail.com>
2026-05-29 20:18:10 +0700

    Update MAINPROJEK.cpp

--- MAINPROJEK.cpp
+++ MAINPROJEK.cpp
@@ -4,9 +4,58 @@
 
 int main ()
 {
-    cout << "Hello world" << endl;
+    system("cls");
 
+    cout << "========================================" << endl;
+    cout << "               HELP MENU" << endl;
+    cout << "========================================" << endl;
+    cout << endl;
 
+    cout << "- Tujuan game mendekati angka 21" << endl;
+    cout << "- Jika lebih dari 21 maka kalah" << endl;
+    cout << "- Dealer otomatis hit jika <17" << endl;
+    cout << "- Pilih HIT untuk tambah kartu" << endl;
+    cout << "- Pilih STAND untuk berhenti" << endl;
 
+    cout << endl;
 
+    cout << "Card Value :" << endl;
+    cout << "A = 1 atau 11" << endl;
+    cout << "J/Q/K = 10" << endl;
+
+    cout << endl;
+
+    system("pause");
+}
+
+void aboutProgram()
+{
+    system("cls");
+
+    cout << "========================================" << endl;
+    cout << "             ABOUT PROGRAM" << endl;
+    cout << "========================================" << endl;
+    cout << endl;
+
+    cout << "BLACKJACK 21" << endl;
+    cout << "Project Algoritma dan Pemrograman" << endl;
+
+    cout << endl;
+
+    cout << "Features :" << endl;
+    cout << "- Struct" << endl;
+    cout << "- Rekursif" << endl;
+    cout << "- Searching" << endl;
+    cout << "- Sorting" << endl;
+    cout << "- Pointer" << endl;
+    cout << "- File I/O" << endl;
+
+    cout << endl;
+
+    cout << "Created By :" << endl;
+    cout << "Muhammad Zenitho Mawla Pasha Munif" << endl;
+
+    cout << endl;
+
+    system("pause");
 }
